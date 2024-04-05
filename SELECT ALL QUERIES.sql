474ca6aa236a6bd2aaebe28a160682f7c687a5f9
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2024-04-05 14:45:37 +0530

    From the following table, write a SQL query that combines the winners in Physics, 1970 and in Economics, 1971. Return year, subject, winner, country, and category.Sample table: nobel_win

--- SELECT ALL QUERIES.sql
+++ SELECT ALL QUERIES.sql
@@ -374,3 +374,48 @@ YEAR SUBJECT                   WINNER                                        COU
 SELECT *
 FROM NOBEL_WIN
 WHERE WINNER LIKE 'Louis %';
+
+
+/*
+19= From the following table, write a SQL query that combines the winners in Physics, 1970 and in Economics, 1971.
+Return year, subject, winner, country, and category. 
+
+Sample table: nobel_win
+
+YEAR SUBJECT                   WINNER                                        COUNTRY                CATEGORY
+---- ------------------------- --------------------------------------------- ------------------------- ------------
+1970 Physics                   Hannes Alfven                                 Sweden                 Scientist
+1970 Physics                   Louis Neel                                    France                 Scientist
+1970 Chemistry                 Luis Federico Leloir                          France                 Scientist
+1970 Physiology                Ulf von Euler                                 Sweden                 Scientist
+1970 Physiology                Bernard Katz                                  Germany                Scientist
+1970 Literature                Aleksandr Solzhenitsyn                        Russia                 Linguist
+1970 Economics                 Paul Samuelson                                USA                    Economist
+1970 Physiology                Julius Axelrod                                USA                    Scientist
+1971 Physics                   Dennis Gabor                                  Hungary                Scientist
+1971 Chemistry                 Gerhard Herzberg                              Germany                Scientist
+1971 Peace                     Willy Brandt                                  Germany                Chancellor
+1971 Literature                Pablo Neruda                                  Chile                  Linguist
+1971 Economics                 Simon Kuznets                                 Russia                 Economist
+1978 Peace                     Anwar al-Sadat                                Egypt                  President
+1978 Peace                     Menachem Begin                                Israel                 Prime Minister
+1987 Chemistry                 Donald J. Cram                                USA                    Scientist
+1987 Chemistry                 Jean-Marie Lehn                               France                 Scientist
+1987 Physiology                Susumu Tonegawa                               Japan                  Scientist
+1994 Economics                 Reinhard Selten                               Germany                Economist
+1994 Peace                     Yitzhak Rabin                                 Israel                 Prime Minister
+1987 Physics                   Johannes Georg Bednorz                        Germany                Scientist
+1987 Literature                Joseph Brodsky                                Russia                 Linguist
+1987 Economics                 Robert Solow                                  USA                    Economist
+1994 Literature                Kenzaburo Oe                                  Japan                  Linguist
+
+*/
+SELECT *
+FROM NOBEL_WIN
+WHERE SUBJECT='Physics' AND Year=1970
+
+UNION
+   
+SELECT *
+FROM NOBEL_WIN
+WHERE SUBJECT='Economics' AND Year=1971;
