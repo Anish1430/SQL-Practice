f95885c630b0ccb02eec0a65e7e7f824b2f1bedc
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2024-04-05 13:41:56 +0530

    From the following table, write a SQL query to find customers whose grade is 200. Return customer_id, cust_name, city, grade, salesman_id. Sample table: customer

--- SELECT ALL QUERIES.sql
+++ SELECT ALL QUERIES.sql
@@ -57,3 +57,22 @@ VALUES
      select name
      from orders where city = "Paris";
  
+ /*
+   10. From the following table, write a SQL query to find customers whose grade is 200. Return customer_id, cust_name, city, grade, salesman_id.
+    Sample table: customer
+
+ customer_id |   cust_name    |    city    | grade | salesman_id 
+-------------+----------------+------------+-------+-------------
+        3002 | Nick Rimando   | New York   |   100 |        5001
+        3007 | Brad Davis     | New York   |   200 |        5001
+        3005 | Graham Zusi    | California |   200 |        5002
+        3008 | Julian Green   | London     |   300 |        5002
+        3004 | Fabian Johnson | Paris      |   300 |        5006
+        3009 | Geoff Cameron  | Berlin     |   100 |        5003
+        3003 | Jozy Altidor   | Moscow     |   200 |        5007
+        3001 | Brad Guzan     | London     |   300 |        5005
+*/
+      select *
+    from customer
+    where grade = 200;
+
